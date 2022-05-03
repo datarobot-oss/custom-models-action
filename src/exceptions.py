0bc99bac62bb90cf39efdb6aca8cd158701f3965
Zohar Mizrahi <zohar.mizrahi@datarobot.com>
2022-05-03 11:14:32 +0300

    [RAPTOR-8034] Integrate Python logger

---
+++ src/exceptions.py
@@ -0,0 +1,9 @@
+
+class GenericException(Exception):
+    def __init__(self, msg, code=-1):
+        super().__init__(msg)
+        self.code = code
+
+
+class InvalidModelSchema(GenericException):
+    pass
