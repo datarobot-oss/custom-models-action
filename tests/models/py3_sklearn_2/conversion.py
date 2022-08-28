76a8316e7a84adcb60809c7793b2f0c099d44727
Zohar Mizrahi <zohar.mizrahi@datarobot.com>
2022-08-28 19:03:56 +0300

    03  move conversion to common (#138)

--- tests/models/py3_sklearn_2/conversion.py
+++
@@ -1,8 +0,0 @@
-#  Copyright (c) 2022. DataRobot, Inc. and its affiliates.
-#  All rights reserved.
-#  This is proprietary source code of DataRobot, Inc. and its affiliates.
-#  Released under the terms of DataRobot Tool and Utility Agreement.
-
-
-def inch_to_cm(inches):
-    return 2.54 * inches
